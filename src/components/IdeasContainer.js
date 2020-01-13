66e496cc5b572b75c1e897aa6accd81a391b5d96
Jan Krupa <jan.krupa@vsb.cz>
2020-01-13 10:39:49 +0100

    Immutability Helper installed
    npm install immutability-helper --save

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Idea from './Idea'
 import axios from 'axios'
+import update from 'immutability-helper'
 
 class IdeasContainer extends Component {
   constructor(props) {
@@ -23,12 +24,39 @@ class IdeasContainer extends Component {
   render() {
     return (
       <div>
-        {this.state.ideas.map((idea) => {
-          return(<Idea idea={idea} key={idea.id} />)
-        })}
+        <div>
+          <button className="newIdeaButton"
+           onClick={this.addNewIdea} >
+            New Idea
+          </button>
+        </div>
+
+        <div>
+          {this.state.ideas.map((idea) => {
+            return(<Idea idea={idea} key={idea.id} />)
+          })}
+        </div>
       </div>
     );
   }
+
+  // Add new idea action
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.log(error))
+  }
+
 }
 
 export default IdeasContainer
