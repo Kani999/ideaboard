3f9f001216a1c2d2a31172084bc9f9ce4dc89be7
Jan Krupa <jan.krupa@vsb.cz>
2020-01-13 13:48:27 +0100

    Makes all stickers editable

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -44,9 +44,11 @@ class IdeasContainer extends Component {
             if(this.state.editingIdeaId === idea.id) {
               return(<IdeaForm idea={idea} key={idea.id}
                       updateIdea={this.updateIdea}
+                      titleRef= {input => this.title = input}
                       resetNotification={this.resetNotification} />)
             } else {
-              return (<Idea idea={idea} key={idea.id} />)
+              return (<Idea idea={idea} key={idea.id}
+                       onClick={this.enableEditing} />)
             }
           })}
         </div>
@@ -94,6 +96,12 @@ class IdeasContainer extends Component {
   resetNotification = () => {
     this.setState({notification: ''})
   }
+
+  enableEditing = (id) => {
+    this.setState({editingIdeaId: id},
+      () => { this.title.focus() })
+  }
+
 }
 
 export default IdeasContainer
